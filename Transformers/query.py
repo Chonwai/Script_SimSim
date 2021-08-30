380097465e7f290ff425692b4366ffdb20dd5ba2
Chonwai <chonwaiun@gmail.com>
2021-08-30 17:54:54 +0800

    Update: Updated the loading model method.

--- Transformers/query.py
+++ Transformers/query.py
@@ -1,9 +1,7 @@
 from sentence_transformers import SentenceTransformer, util
-import torch
 import argparse
 import pickle
-
-print(torch.cuda.is_available())
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--sentence', default='我每天佩戴手錶坐巴士上學！',
@@ -19,7 +17,7 @@ model = SentenceTransformer('distiluse-base-multilingual-cased-v2')
 
 #Load sentences & embeddings from disc
 with open('../embeddings/embeddings.pkl', "rb") as fIn:
-    stored_data = torch.load(fIn, map_location=torch.device('cpu'))
+    stored_data = pickle.load(fIn)
     stored_sentences = stored_data['sentences']
     stored_embeddings = stored_data['embeddings']
 
@@ -28,8 +26,14 @@ query_embeddings = model.encode(query, convert_to_tensor=True)
 #Compute cosine-similarities for each sentence with each other sentence
 cosine_scores = util.pytorch_cos_sim(stored_embeddings, query_embeddings)
 
-print(cosine_scores[0:20])
+score_list = list(cosine_scores)
+
+top_10_idx = np.argsort(score_list)[-10:]
+score = 0
 
 # Output the pairs with their score
-for i in range(len(stored_sentences[0:20])):
-    print("{}...... \t\t {} \t\t Score: {:.4f} \n".format(stored_sentences[i][0:15], query[0], cosine_scores[i][0]))
+for i in top_10_idx:
+    print("{}...... \t\t {} \t\t Score: {:.4f}".format(stored_sentences[i][0:15], query[0], cosine_scores[i][0]))
+    score = score + cosine_scores[i][0]
+
+print("\n{}\tScore: {}".format(query[0], score/len(top_10_idx)))
